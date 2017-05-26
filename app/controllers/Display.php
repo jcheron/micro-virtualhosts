4eec458a1cdd7b2519854d7e79e92e0150dae7bf
Rick_Sanchez <constantin.pavlovic@gmail.com>
2017-05-26 19:15:03 +0200

    Add files via upload
    fonctionnel mais incomplet

--- app/controllers/Display.php
+++ app/controllers/Display.php
@@ -36,41 +36,52 @@ class Display extends ControllerBase
 		$vHost=DAO::getOne("models\Virtualhost","id=".$idvirtualhost);		
 		$vHostProperty=DAO::getOne("models\Virtualhostproperty","idVirtualhost=".$idvirtualhost);
 		
-		if ( isset ( $vHostProperty['value'] ) )
+		
+		
+		
+		
+		
+		
+		if ( isset($vHostProperty) ) /*TEST POUR SAVOIR SI LE VHOST POSSEDE DES PROPERTIES*/
 		{
+			
 			$vhP_value = $vHostProperty->getValue();
+			$vhP_active = $vHostProperty->getActive();
+			$vhP_comment = $vHostProperty->getComment();
+			
+			
+			
+			$vHost_prop_value=DAO::getAll("models\VirtualhostProperty","idvirtualhost=".$idvirtualhost);
+			$property=DAO::getAll("models\Property");
+			
+			$vHost_prop = 
+			[
+					$property[0]->__toString()=>$vHost_prop_value[0]->__toString(),
+					$property[1]->__toString()=>$vHost_prop_value[1]->__toString(),
+					$property[2]->__toString()=>$vHost_prop_value[2]->__toString(),
+					$property[3]->__toString()=>$vHost_prop_value[3]->__toString()
+			];
+
 		}
 		else 
 		{
 			$vhP_value = "N/A";
-		}
-		
-		if ( isset ( $vHostProperty['active'] ) )
-		{
-			$vhP_active = $vHostProperty->getActive();
-		}
-		else
-		{
 			$vhP_active = 0;
+			$vhP_comment = 0;
+			$vHost_prop = "";
 		}
 		
-		if ( isset ( $vHostProperty['comment'] ) )
-		{
-			$vhP_comment = $vHostProperty->getComment();
-		}
-		else
-		{
-			$vhP_comment = 0;
-		}
+		
+		
 		
 		
 		$this->jquery->compile($this->view);
-		$this->loadView("Display/index.html",array("id_vhost"=>$idvirtualhost,"nom_vhost"=>$vHost->getName(),"config_vhost"=>$vHost->getConfig(),"id_serveur_vhost"=>$vHost->getServer(),"vhP_value"=>$vhP_value,"vhP_active"=>$vhP_active,"vhP_comment"=>$vhP_comment));
+		$this->loadView("Display/index.html",array(
+			"id_vhost"=>$idvirtualhost,"nom_vhost"=>$vHost->getName(),"config_vhost"=>$vHost->getConfig(),"id_serveur_vhost"=>$vHost->getServer()
+			,"vhP_value"=>$vhP_value,"vhP_active"=>$vhP_active,"vhP_comment"=>$vhP_comment
+			,"vHost_prop"=>$vHost_prop ));
 		
 	}
-	
-	
-	
 	
 	
 }
